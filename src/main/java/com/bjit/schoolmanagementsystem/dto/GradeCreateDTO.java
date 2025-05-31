c4c44ed6a9691077d0ed53ac945a31b63f7bb5fc
rakib <aktaruzzaman.rakib@bjitgroup.com>
2025-05-31 16:35:11 +0600

    used appropriate validation import

--- src/main/java/com/bjit/schoolmanagementsystem/dto/GradeCreateDTO.java
+++ src/main/java/com/bjit/schoolmanagementsystem/dto/GradeCreateDTO.java
@@ -3,9 +3,9 @@ package com.bjit.schoolmanagementsystem.dto;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 @Setter
 @Getter
